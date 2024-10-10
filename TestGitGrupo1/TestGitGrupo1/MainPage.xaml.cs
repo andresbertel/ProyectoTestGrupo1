6a8bef0eeb74dc2bf9e36afc27bf013e588d5198
albeirovergara <albeirojosevergaraperez@gmail.com>
2024-10-10 07:39:25 -0500

    Se agrego la la nueva pagina con su nuevo boton

--- TestGitGrupo1/TestGitGrupo1/MainPage.xaml.cs
+++ TestGitGrupo1/TestGitGrupo1/MainPage.xaml.cs
@@ -9,21 +9,15 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+     
 
         private void Button_Clicked(object sender, EventArgs e)
         {
 
+        }
+        private void Page_To_Real_Madrid(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page_Real_Madrid());
         }
     }
 
---
+++ TestGitGrupo1/TestGitGrupo1/Page_Real_Madrid.xaml.cs
@@ -0,0 +1,9 @@
+namespace TestGitGrupo1;
+
+public partial class Page_Real_Madrid : ContentPage
+{
+	public Page_Real_Madrid()
+	{
+		InitializeComponent();
+	}
+}
